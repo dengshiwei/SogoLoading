2f333298b0e88458bb5bd9a23256b76c59434af0
dengshiwei <dengshiwei@sensorsdata.cn>
2021-03-17 18:27:24 +0800

    release

--- app/src/main/java/com/antway/sogoloadingproject/MainActivity.java
+++ app/src/main/java/com/antway/sogoloadingproject/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.antway.sogoloading.SoGouBrowserLoading;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        SoGouBrowserLoading soGouBrowserLoading = new SoGouBrowserLoading(this,null);
+        setContentView(soGouBrowserLoading);
     }
 }
